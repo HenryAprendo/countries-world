6cdf292bd77f329511f1dbe7da5797b3a2107f01
HenryAprendo <hjsmipuc@gmail.com>
2023-06-27 12:57:06 -0500

    Configuración inicial del proyecto, instalación de tailwind, assets, fonts, colors

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      'dark-blue-elements-dm': '#2B3945', //dark mode elements
+      'very-dark-blue-background-dm': '#202C37', //dark mode background
+      'very-dark-blue-text-lm': '#111517',  //light mode text
+      'dark-gray-input-lm': '#858585',  //light mode input
+      'very-light-gray-background-lm': '#FAFAFA', //light mode background
+      'white-text-dm': '#FFFFFF', //dark mode text
+      'white-elements-lm': '#FFFFFF' //light mode elements
+    },
+
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
