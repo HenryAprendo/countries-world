ff578a0e348e15151c653d5fc9b680a94426c849
HenryAprendo <hjsmipuc@gmail.com>
2023-06-28 11:50:41 -0500

    Obteniendo el elemento raiz html para agregarle o removerle la clase dark de acuerdo al modo seleccionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DarkModeService } from './services/dark-mode.service';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -14,20 +15,30 @@ export class AppComponent implements OnInit {
 
   private darkModeService:DarkModeService = inject(DarkModeService);
 
+  private render:Renderer2 = inject(Renderer2);
+
   private darkModeActual:string = '';
 
   ngOnInit(): void {
-    this.darkModeService.selectedMode
-      .subscribe(value => {
-        this.darkModeActual = value;
-        document.documentElement.className = value;
-      });
+    this.applyModeSelected();
   }
 
   toogleMode(){
     this.darkModeActual === 'dark'
       ? this.darkModeService.setMode('light')
       : this.darkModeService.setMode('dark');
+  }
+
+  private applyModeSelected() {
+    this.darkModeService.selectedMode
+    .subscribe(value => {
+      this.darkModeActual = value;
+      let rootElement:HTMLElement = document.documentElement;
+
+      value === 'dark'
+        ? this.render.addClass(rootElement,value)
+        : this.render.removeClass(rootElement,'dark');
+    });
   }
 
 }
