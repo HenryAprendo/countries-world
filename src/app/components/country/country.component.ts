a00808afd5c71402131c3be8c555a7c657797648
HenryAprendo <hjsmipuc@gmail.com>
2023-07-03 16:55:52 -0500

    Implementación de carga de imagenes con lazi loading con la directiva NgOptimizedImage

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Country } from './../../model/country.model';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-country',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgOptimizedImage],
   templateUrl: './country.component.html',
   styles: [
   ],
